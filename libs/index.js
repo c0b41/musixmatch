5026d12a4a09d5a0d36706202cb3c37a5a6f2097
cobaimelan <ayhankuru@yandex.com.tr>
2014-06-23 21:26:50 +0300

    fix

--- libs/index.js
+++ libs/index.js
@@ -9,13 +9,16 @@ var qp = require('query-parse');
 
 /**
  * new musixmatch
- * @param {str} object
+ * @param {obj} object
  */
 function musixmatch(obj){
 	if (!(this instanceof musixmatch)) return new musixmatch(obj);
-  	this.usertoken = obj.usertoken || '1385437af7222f2b5ec105bf0b456fed7bdf84066ad62b25';
-  	this.app_id =obj.app_id || 'community-app-v1.0';
-  	this.method = obj.method || 'json'; 
+	var obj =obj ? obj : {};
+  	this.usertoken =obj.usertoken ? obj.usertoken :"1385437af7222f2b5ec105bf0b456fed7bdf84066ad62b25";
+  	this.app_id = obj.app_id ? obj.app_id : "community-app-v1.0";
+  	this.method = obj.method ? obj.method : "json"; 
+
+  	return this;
 }
 
 /**
@@ -24,7 +27,7 @@ function musixmatch(obj){
  */
 musixmatch.prototype.artist = function(str){
 	if( typeof str =="object" ){
-		this.url ="https://community.musixmatch.com/ws/1.1/artist.search?app_id="+this.app_id+"&usertoken="+this.usertoken+"&format="+this.method+"&"+qp.toString(str);
+		this.url ="https://community.musixmatch.com/ws/1.1/artist.search?app_id="+this.app_id+"&usertoken="+this.usertoken+"&format="+this.method+"&part=artist_image&s_artist_ranking=desc&didyoumean=1&"+qp.toString(str);
 	}else{
 		this.url=null;
 	}
@@ -39,7 +42,7 @@ musixmatch.prototype.artist = function(str){
 
 musixmatch.prototype.track = function(str){
 	if( typeof str =="object" ){
-		this.url ="https://community.musixmatch.com/ws/1.1/track.search?app_id="+this.app_id+"&usertoken="+this.usertoken+"&format="+this.method+"&"+qp.toString(str);
+		this.url ="https://community.musixmatch.com/ws/1.1/track.search?app_id="+this.app_id+"&f_stop_words=1&s_track_rating=desc&g_common_track=1&usertoken="+this.usertoken+"&format="+this.method+"&"+qp.toString(str);
 	}else{
 		this.url=null;
 	}
@@ -49,7 +52,7 @@ musixmatch.prototype.track = function(str){
 
 /**
  *  exec request
- * @param {fn}  function
+ * @param {next}  function
  */
 musixmatch.prototype.exec = function(next){
 
@@ -71,3 +74,4 @@ musixmatch.prototype.exec = function(next){
 
 
 module.exports=exports=musixmatch;
+
