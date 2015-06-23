6d2ce5fb4d65f030e40192250110b672b4b5c51c
ayhankuru <ayhankuru@yandex.com.tr>
2015-06-23 19:41:22 +0300

    test close

--- test.js
+++ test.js
@@ -5,14 +5,14 @@ describe('Musixmatch Test!!!...', function(){
 
 		it('Artist search ', function(){
 
-		return music.trackSearch({q:"Tove lo",page:1,page_size:10}).then(function(data){
+		//return music.trackSearch({q:"Tove lo",page:1,page_size:10}).then(function(data){
 				//expect(data).to.be.an('object'); 
 				//expect(data.message.body.track_list[0].track.track_name).to.eql('Got Love'); 
 				//expect(data.message.body.track_list[0].track.album_name).to.eql('Queen of the Clouds'); 
 				//expect(data.message.body.track_list[0].track.artist_name).to.eql('Tove Lo'); 
 				//expect(data.message.body.track_list[0].track.commontrack_vanity_id).to.eql('Tove-Lo/Got-Love');
 				
-			});
+		//	});
 
 		});		
 });
