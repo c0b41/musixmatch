b619fe00f4374f3290186d0ba2afcf74ce54bf74
ayhankuru <ayhankuru@yandex.com.tr>
2018-05-20 00:41:12 +0300

    clear cache

--- example.js
+++
@@ -1,7 +0,0 @@
-var music = require('./dist/index.js')({apikey:'e7a3543c609d7dc2b4499437567b4d33'});
- 
-music.chartArtists({country:"TR",page:1, page_size:10}).then(function(data){
-		console.log(data);
-}).catch(function(err){
-		console.log(err.stack);
-});
